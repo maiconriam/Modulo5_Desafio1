bf97321fae650d4a58e3d0bb28b18783b4ebdfcd
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-11-12 20:08:48 -0300

    Criar validacao para campos obrigatorios

---
+++ src/main/java/br/com/zup/GerenciamentoDeContas/config/ControllerAdvice.java
@@ -0,0 +1,2 @@
+package br.com.zup.GerenciamentoDeContas.config;public class ControllerAdvice {
+}
--- src/main/java/br/com/zup/GerenciamentoDeContas/conta/dtos/CadastrarDTO.java
+++ src/main/java/br/com/zup/GerenciamentoDeContas/conta/dtos/CadastrarDTO.java
@@ -2,13 +2,23 @@ package br.com.zup.GerenciamentoDeContas.conta.dtos;
 
 import br.com.zup.GerenciamentoDeContas.conta.enuns.Tipo;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 public class CadastrarDTO {
 
+    @Size(min = 2, max = 50,message = "Por favor, digite um nome entre 2 a 50 caracteres")
+    @NotBlank(message = "Campo Obrigatorio")
     private String nome;
+    @Positive(message = "Numero invalido")
+    @NotNull(message = "campo obrigatorio")
     private Double valor;
+    @NotNull(message = "Campo Obrigatorio")
     private Tipo tipo;
+    @NotNull(message = "Campo Obrigatorio")
     private LocalDate dataDeVencimento;
 
     public CadastrarDTO() {
