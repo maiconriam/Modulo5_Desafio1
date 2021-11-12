dcd5a8001a684e02aa3b40ad1f9cba3c113e15cd
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-11-12 19:45:12 -0300

    Criar classe de controller

--- src/main/java/br/com/zup/GerenciamentoDeContas/conta/ContaController.java
+++ src/main/java/br/com/zup/GerenciamentoDeContas/conta/ContaController.java
@@ -1,2 +1,13 @@
-package br.com.zup.GerenciamentoDeContas.conta;public class ContaController {
+package br.com.zup.GerenciamentoDeContas.conta;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/contas")
+public class ContaController {
+
+    @Autowired
+    private ContaService contaService;
 }
