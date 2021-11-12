5da3ea48fd6442622287c8bf895ed5dd7516aaf0
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-11-12 20:09:25 -0300

    Criar classe mensagem de erro para exececao

--- src/main/java/br/com/zup/GerenciamentoDeContas/config/MensagemDeErro.java
+++ src/main/java/br/com/zup/GerenciamentoDeContas/config/MensagemDeErro.java
@@ -1,4 +1,17 @@
 package br.com.zup.GerenciamentoDeContas.config;
 
-public class MensagemErro {
+public class MensagemDeErro {
+    private String mensagemDeErro;
+
+    public MensagemDeErro(String mensagemDeErro) {
+        this.mensagemDeErro = mensagemDeErro;
+    }
+
+    public String getMensagemDeErro() {
+        return mensagemDeErro;
+    }
+
+    public void setMensagemDeErro(String mensagemDeErro) {
+        this.mensagemDeErro = mensagemDeErro;
+    }
 }
