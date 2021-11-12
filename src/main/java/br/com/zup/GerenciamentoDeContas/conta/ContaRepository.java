92f10e1fa9abce1bdd8da0628dfee131c99513d0
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-11-12 20:44:33 -0300

    Criar metodo na repository para procurar pelo valor aproximado

--- src/main/java/br/com/zup/GerenciamentoDeContas/conta/ContaRepository.java
+++ src/main/java/br/com/zup/GerenciamentoDeContas/conta/ContaRepository.java
@@ -2,6 +2,7 @@ package br.com.zup.GerenciamentoDeContas.conta;
 
 import br.com.zup.GerenciamentoDeContas.conta.enuns.Status;
 import br.com.zup.GerenciamentoDeContas.conta.enuns.Tipo;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
 public interface ContaRepository extends CrudRepository<Conta, Integer> {
     List<Conta> findAllByStatus(Status status);
     List<Conta> findAllByTipo(Tipo tipo);
+    @Query(value = "SELECT * FROM contas WHERE VALOR BETWEEN :valor-10 AND :valor+10", nativeQuery = true)
+    List<Conta> findAllByValorAproximado(Double valor);
 }
