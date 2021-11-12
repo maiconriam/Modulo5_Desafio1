244ee6dc7e1335bf23910ab0909922c11aafde60
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-11-12 20:26:13 -0300

    Criar metodo para tratar a excpetion personalizada

--- src/main/java/br/com/zup/GerenciamentoDeContas/config/ControllerAdvice.java
+++ src/main/java/br/com/zup/GerenciamentoDeContas/config/ControllerAdvice.java
@@ -1,6 +1,10 @@
 package br.com.zup.GerenciamentoDeContas.config;
 
+import br.com.zup.GerenciamentoDeContas.conta.exception.ContaJaPagaException;
+import br.com.zup.GerenciamentoDeContas.conta.exception.ContaNaoEncontradaException;
+import br.com.zup.GerenciamentoDeContas.conta.exception.StatusErradoParaAtualizarContaException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,5 +27,29 @@ public class ControllerAdvice {
             erros.add(erroDeValidacao);
         }
         return erros;
+    }
+
+    @ExceptionHandler(ContaNaoEncontradaException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public MensagemDeErro manipularExececaoParaContaNaoEncontrada(ContaNaoEncontradaException exception){
+        return new MensagemDeErro(exception.getMessage());
+    }
+
+    @ExceptionHandler(StatusErradoParaAtualizarContaException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public MensagemDeErro manipularExececaoParaStatusErrado(StatusErradoParaAtualizarContaException exception){
+        return new MensagemDeErro(exception.getMessage());
+    }
+
+    @ExceptionHandler(ContaJaPagaException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public MensagemDeErro manipularExececaoParaContaJaPaga(ContaJaPagaException exception){
+        return new MensagemDeErro(exception.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public MensagemDeErro manipularExececaoEnumErrado (HttpMessageNotReadableException exception){
+        return new MensagemDeErro("Erro de Sintaxe");
     }
 }
