bbf164b5e7adc064e8ed993e4445c52c3e78af29
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-11-12 19:47:28 -0300

    Criar classe conversor para model mapper

---
+++ src/main/java/br/com/zup/GerenciamentoDeContas/conta/componentes/Conversor.java
@@ -0,0 +1,13 @@
+package br.com.zup.GerenciamentoDeContas.conta.componentes;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Conversor {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
