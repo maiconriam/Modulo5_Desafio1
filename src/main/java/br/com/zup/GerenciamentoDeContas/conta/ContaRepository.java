2ff228ce31100129649c1f4bdbb663a8357803a6
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-11-12 20:34:45 -0300

    Criar metodo na repository para procurar pelo status especifico

--- src/main/java/br/com/zup/GerenciamentoDeContas/conta/ContaRepository.java
+++ src/main/java/br/com/zup/GerenciamentoDeContas/conta/ContaRepository.java
@@ -1,7 +1,10 @@
 package br.com.zup.GerenciamentoDeContas.conta;
 
+import br.com.zup.GerenciamentoDeContas.conta.enuns.Status;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ContaRepository extends CrudRepository<Conta, Integer> {
+import java.util.List;
 
+public interface ContaRepository extends CrudRepository<Conta, Integer> {
+    List<Conta> findAllByStatus(Status status);
 }
