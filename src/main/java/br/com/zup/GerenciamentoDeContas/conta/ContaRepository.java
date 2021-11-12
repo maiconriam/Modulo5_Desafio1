81ca8579127c6f195877a97fef97553e7f2e14f0
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-11-12 20:42:23 -0300

    Criar metodo na repository para procurar pelo tipo especifico

--- src/main/java/br/com/zup/GerenciamentoDeContas/conta/ContaRepository.java
+++ src/main/java/br/com/zup/GerenciamentoDeContas/conta/ContaRepository.java
@@ -1,10 +1,12 @@
 package br.com.zup.GerenciamentoDeContas.conta;
 
 import br.com.zup.GerenciamentoDeContas.conta.enuns.Status;
+import br.com.zup.GerenciamentoDeContas.conta.enuns.Tipo;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface ContaRepository extends CrudRepository<Conta, Integer> {
     List<Conta> findAllByStatus(Status status);
+    List<Conta> findAllByTipo(Tipo tipo);
 }
