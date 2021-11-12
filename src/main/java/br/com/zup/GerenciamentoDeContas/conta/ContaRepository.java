b3d01ddef245a7bb47cfb63f483997c2e93cd862
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-11-12 19:41:26 -0300

    Criar classe para repository

---
+++ src/main/java/br/com/zup/GerenciamentoDeContas/conta/ContaRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.GerenciamentoDeContas.conta;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContaRepository extends CrudRepository<Conta, Integer> {
+
+}
