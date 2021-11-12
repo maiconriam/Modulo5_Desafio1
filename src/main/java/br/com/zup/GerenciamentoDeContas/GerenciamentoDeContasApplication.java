e7734d970c75872eae9a799b397375a1aee525a2
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-11-12 19:20:33 -0300

    Primeiro commit

---
+++ src/main/java/br/com/zup/GerenciamentoDeContas/GerenciamentoDeContasApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.GerenciamentoDeContas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeContasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/GerenciamentoDeContas/GerenciamentoDeContasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.GerenciamentoDeContas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
