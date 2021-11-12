24802391e69b9acaac95093e4b10ca0631649fff
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-11-12 20:11:31 -0300

    Criar metodo para tratar exececao de validacao

--- src/main/java/br/com/zup/GerenciamentoDeContas/config/ControllerAdvice.java
+++ src/main/java/br/com/zup/GerenciamentoDeContas/config/ControllerAdvice.java
@@ -1,9 +1,27 @@
 package br.com.zup.GerenciamentoDeContas.config;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class ControllerAdvice {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public List<MensagemDeErro> manipularErrosDeValidacao(MethodArgumentNotValidException exception){
+        List<MensagemDeErro> erros = new ArrayList<>();
 
+        for (FieldError fieldError : exception.getFieldErrors()){
+            MensagemDeErro erroDeValidacao = new MensagemDeErro(fieldError.getDefaultMessage());
+            erros.add(erroDeValidacao);
+        }
+        return erros;
+    }
 }
